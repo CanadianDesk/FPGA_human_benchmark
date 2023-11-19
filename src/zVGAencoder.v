f0ea03b6765fd2364fedb7276c26d9f9e9714389
Avinav <avinavbhandari@gmail.com>
2023-11-18 22:20:46 -0500

    Update zVGAencoder.v

--- src/zVGAencoder.v
+++ src/zVGAencoder.v
@@ -3,7 +3,7 @@
 //TOP LEVEL MODULE FOR VGA ENCODER
 
 //NEEDS INPUTS:
-//1) From mainMenu a 2 bit in put based on what is being displayed 0-menu, 1-react, 2-chimp
+////1: 2 bits, 00 if Menu, 01 of React, 10 if Chimp, 11 if somethings fucked up 
 
 
 module zVGAencoder (
