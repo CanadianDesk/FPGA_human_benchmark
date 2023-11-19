119ea0f9b6ccaa10839e8502fe3b4f287b385988
Jonah Diamond <jonahdiamond2@gmail.com>
2023-11-18 22:51:35 -0500

    updated main menu ctrl path and removed mainMenuControlPath file

--- src/ZmainMenu.v
+++ src/ZmainMenu.v
@@ -1,4 +1,4 @@
-// The main module of the main menu FSM
+// Main Menu Control Path
 // Jonah Diamond
 // ECE241 Project
 
@@ -6,21 +6,40 @@
 //// 1. 1 bit, high if 1 on the PS2 is pressed, low if not pressed
 //// 2. 1 bit, high if 2 on the PS2 is pressed, low if not pressed
 // OUTPUTS
-//// 1. 2 bit, output of which mode to display (00 menu, 01 react, 10 chimp, 11 default)
+//// 1. 2 bit, output of which mode to display (00 menu, 01 react, 10 chimp)
 
-// Internal needs:
-////
+module zMainMenu(input i1, i2, iKey0, clk,
+    output [1:0] oMode);
+    localparam MENU = 2'd0,
+    REACT_WAIT = 2'd1,
+    REACT = 2'd2,
+    CHIMP_WAIT = 2'd3,
+    CHIMP = 2'd4,
+    MENU_WAIT = 2'd5;
+    always@(*) begin
+        case (current_state) 
+        MENU: begin
+            oMode = 2'd0;
+            if (i1) next_state = REACT_WAIT;
+            if (i2) next_state = CHIMP_WAIT;
+            end
+        REACT_WAIT: next_state = i1 ? REACT_WAIT : REACT;
+        CHIMP_WAIT: next_state = 12 ? CHIMP_WAIT : CHIMP;
+        REACT: begin 
+            next_state = iKey0 ? MENU_WAIT : REACT;
+            oMode = 2'd1;
+            end
+        CHIMP: begin
+            next_state = iKey0 ? MENU_WAIT : CHIMP;
+            oMode = 2'd2;
+            end
+        MENU_WAIT: next_state = iKey0 ? MENU_WAIT : MENU;
+        default: next_state = MENU;
+        endcase
+    end
 
-//module mainMenu(clk, i1, i2, iKey0, oDisplayChoice)
-module mainMenu(iClock, iReset, iW, iUserChoice, oZ, oCurState);
-    input iClock, iReset, iW;
-    input [1:0] iUserChoice;
-    output iZ;
-    output reg [3:0] oCurState;
-    reg [3:0] y_Q, Y_D;
-    localparam Menu = 2'b00, React = 2'b01, Chimp = 2'b10;
-    
-
-        
-    
+    always@(posedge clk) begin
+        current_state = next_state;
+    end    
 endmodule
+
---
+++ tests/zMainMenu.do
@@ -0,0 +1,8 @@
+vlib work
+
+vlog zMainMenu.v
+vsim zMainMenu
+
+log -r /*
+
+add wave -r /*
