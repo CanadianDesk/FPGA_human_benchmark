b977154a201dfe1ac2e05f79010a5c23ad6a1cc0
Darsan Qi <darsan.qitian@gmail.com>
2023-11-26 18:23:54 -0500

    renamed stuff

--- src/Keyboard/zTopLevelKeyboard.v
+++ src/Keyboard/zTopLevelKeyboard.v
@@ -11,6 +11,5 @@ module zTopLevelKeyboard(CLOCK_50, KEY, PS2_CLK, PS2_DAT, HEX, LEDR)
 
     hex_display u0(received_data[3:0], HEX0);
     hex_display u0(received_data[7:4], HEX1);
-
     PS2_Controller u0(CLOCK_50, KEY0, PS2_CLK, PS2_DAT, received_data, LEDR[4], LEDR[3], LEDR[2], LEDR[1], LEDR[0]);
 endmodule
