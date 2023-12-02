b99aa8a1df76c8554e41d6627866b60f95518fa9
Darsan Qi <darsan.qitian@gmail.com>
2023-12-02 00:42:04 -0500

    made a top level for keyboard and mouse

---
+++ src/MouseWithKeyboard/zUltimateTopLevel.v
@@ -0,0 +1,18 @@
+module mouseWithKeyboard(
+	input CLOCK_50,
+	input KEY,
+	// Bidirectionals
+	input PS2_CLK,
+	input PS2_DAT,
+	output one_pressed,
+	output two_pressed,
+	output space_pressed,
+	output leftClick_pressed,
+	output [9:0] x_position,
+	output [8:0] y_position
+);
+
+zTopLevelKeyboard keyboard(CLOCK_50, KEY, PS2_CLK, PS2_DAT, space_pressed, enter_pressed, one_pressed, two_pressed);
+zTopLevelMouse mouse(CLOCK_50, KEY, PS2_CLK, PS2_DAT, x_position, y_position, leftClick_pressed);
+
+endmodule
