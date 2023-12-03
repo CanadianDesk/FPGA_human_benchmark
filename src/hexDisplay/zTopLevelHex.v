0f7ab803ec8d1d74f535524788cbc48a2f6cd520
Darsan Qi <darsan.qitian@gmail.com>
2023-12-02 22:39:51 -0500

    made the hex displays for chimp and react and menu

--- src/hexDisplay/zTopLevelHex.v
+++ src/hexDisplay/zTopLevelHex.v
@@ -1,9 +1,10 @@
 module zTopLevelHex(
     input CLOCK_50,
-    input gameMode,
-    input reactScore,
-    input chimpScore,
-    input chimpLives,
+    input mode,
+    input [11:0]reactCurrentScore,
+    input [11:0]reactHighScore,
+    input screen,
+    input [4:0] chimpLevel,
     output [6:0] HEX0,
     output [6:0] HEX1,
     output [6:0] HEX2,
@@ -14,71 +15,76 @@ module zTopLevelHex(
     output [6:0] HEX7
 );
 
-reg [2:0] current_state, next_state;
+reg [3:0] display0;
+reg [3:0] display1;
+reg [3:0] display2;
+reg [3:0] display3;
 
-
-localparam	CHIMP = 2'b00,
-			REACTION = 2'b01,
+// Internal variables
+integer decimal_value;
+integer thousands, hundreds, tens, ones;
 	
+initial 
+begin
+    display0 = 0;
+    display1 = 0;
+    display2 = 0;
+    display3 = 0;
+
+end
+
 always @(posedge CLOCK_50)
 begin
-	if (reset == 1'b1)
-		s_ps2_receiver <= PS2_STATE_0_IDLE;
-	else
-		s_ps2_receiver <= ns_ps2_receiver;
+    if(mode == 0)
+    begin
+        display0 = 0;
+        display1 = 0;
+        display2 = 0;
+        display3 = 0;
+        //do nothing
+    end
+    else if(mode == 1) //react
+    begin
+        if(screen == 3)
+        begin
+            decimal_value = reactCurrentScore; // Interpret binary as decimal
+            thousands = decimal_value / 1000;
+            hundreds = (decimal_value % 1000) / 100;
+            tens = (decimal_value % 100) / 10;
+            ones = (decimal_value % 10);
+            display0 = ones;
+            display1 = tens;
+            display2 = hundreds;
+            display3 = thousands;
+        end
+        else
+        begin
+            decimal_value = reactHighScore; // Interpret binary as decimal
+            thousands = decimal_value / 1000;
+            hundreds = (decimal_value % 1000) / 100;
+            tens = (decimal_value % 100) / 10;
+            ones = (decimal_value % 10);
+            display0 = ones;
+            display1 = tens;
+            display2 = hundreds;
+            display3 = thousands;
+        end
+    end
+    else if(mode == 2) // chimp
+    begin
+        decimal_value = chimpLevel; // Interpret binary as decimal
+        tens = (decimal_value % 100) / 10;
+        ones = (decimal_value % 10);
+        display0 = ones;
+        display1 = tens;
+        display2 = 0;
+        display3 = 0;
+    end
 end
 
-always @(*)
-begin
-	// Defaults
-	ns_ps2_receiver = PS2_STATE_0_IDLE;
-    case (s_ps2_receiver)
-	PS2_STATE_0_IDLE:
-		begin
-			if ((wait_for_incoming_data == 1'b1) && 
-					(received_data_en == 1'b0))
-				ns_ps2_receiver = PS2_STATE_1_WAIT_FOR_DATA;
-			else if ((start_receiving_data == 1'b1) && 
-					(received_data_en == 1'b0))
-				ns_ps2_receiver = PS2_STATE_2_DATA_IN;
-			else
-				ns_ps2_receiver = PS2_STATE_0_IDLE;
-		end
-	PS2_STATE_1_WAIT_FOR_DATA:
-		begin
-			if ((ps2_data == 1'b0) && (ps2_clk_posedge == 1'b1))
-				ns_ps2_receiver = PS2_STATE_2_DATA_IN;
-			else if (wait_for_incoming_data == 1'b0)
-				ns_ps2_receiver = PS2_STATE_0_IDLE;
-			else
-				ns_ps2_receiver = PS2_STATE_1_WAIT_FOR_DATA;
-		end
-	PS2_STATE_2_DATA_IN:
-		begin
-			if ((data_count == 3'h7) && (ps2_clk_posedge == 1'b1))
-				ns_ps2_receiver = PS2_STATE_3_PARITY_IN;
-			else
-				ns_ps2_receiver = PS2_STATE_2_DATA_IN;
-		end
-	PS2_STATE_3_PARITY_IN:
-		begin
-			if (ps2_clk_posedge == 1'b1)
-				ns_ps2_receiver = PS2_STATE_4_STOP_IN;
-			else
-				ns_ps2_receiver = PS2_STATE_3_PARITY_IN;
-		end
-	PS2_STATE_4_STOP_IN:
-		begin
-			if (ps2_clk_posedge == 1'b1)
-				ns_ps2_receiver = PS2_STATE_0_IDLE;
-			else
-				ns_ps2_receiver = PS2_STATE_4_STOP_IN;
-		end
-	default:
-		begin
-			ns_ps2_receiver = PS2_STATE_0_IDLE;
-		end
-	endcase
-end
+decimal_To_Seven_Segment unit0(display0, HEX0);
+decimal_To_Seven_Segment unit1(display1, HEX1);
+decimal_To_Seven_Segment unit2(display2, HEX2);
+decimal_To_Seven_Segment unit3(display3, HEX3);
 
 endmodule
